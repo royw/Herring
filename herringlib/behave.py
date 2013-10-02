a17ebda9eecd97337a7d9166f20e33071ab343f7
Roy Wright <roy@wright.org>
2013-10-02 10:18:35 -0500

    corrected reference to Project

--- herringlib/behave.py
+++ herringlib/behave.py
@@ -5,14 +5,15 @@ Helper for running the behave BDD utility.
 """
 from herring.herring_app import task
 from herring.herring_file import HerringFile
-from herringlib.runner import script
-from herringlib.setup_tasks import Project
 
 required_packages = [
     'behave'
 ]
 
 if HerringFile.packagesRequired(required_packages):
+    from herringlib.runner import script
+
+    global Project
 
     @task(help='You may append behave options when invoking the features task.')
     def features():
