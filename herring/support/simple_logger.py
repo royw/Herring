3f39f90ddb0d3be8bcac0a82fd75be74d45b1851
royw <roy@wright.org>
2014-08-26 12:19:02 -0500

    backport from herringlib project

--- herring/support/simple_logger.py
+++ herring/support/simple_logger.py
@@ -15,6 +15,7 @@ STEP = '.'
 
 class FileLogger(object):
     """ Very basic file logger class that simply appends messages to a file """
+
     def __init__(self, filename):
         self.filename = filename
         fh = open(self.filename, 'w')
@@ -154,6 +155,31 @@ class SimpleLogger(object):
         """
         self.show_level = show_level
 
+    def _output_prefix(self, level):
+        """
+        generate the prefix for a log message
+
+        :param level: the log level ('debug', 'info', 'warning', 'error', 'fatal')
+        :type level: str
+        :returns: the prefix for a log message
+        :rtype: str
+        """
+
+        now = time()
+        diff_time = now - self.last_message_time
+        self.last_message_time = now
+
+        buf = []
+        if self.enable_timestamp:
+            buf.append("{now} ".format(now=strftime("%H:%M:%S", localtime(now))))
+        if self.enable_elapsed_time:
+            buf.append("%.3f " % diff_time)
+        if self.show_level:
+            buf.append("{level}:  ".format(level=level.upper()))
+        if self.current_component:
+            buf.append("[{component}]  ".format(component=str(self.current_component)))
+        return ''.join(buf)
+
     def _output(self, level, message, newline=True):
         """
         Assemble the message and send it to the appropriate stream(s).
@@ -177,26 +203,16 @@ class SimpleLogger(object):
         :param level: the log level ('debug', 'info', 'warning', 'error', 'fatal')
         :type level: str
         :param message: the message to include in the output message.
-        :type message: str|Exception
+        :type message: str
         :param newline: if asserted then append a newline to the end of the message
         :type newline: bool
         """
-        now = time()
-        diff_time = now - self.last_message_time
-        self.last_message_time = now
 
         buf = []
         if newline and not self.previous_newline:
             buf.append("\n")
         self.previous_newline = newline
-        if self.enable_timestamp:
-            buf.append("{now} ".format(now=strftime("%H:%M:%S", localtime(now))))
-        if self.enable_elapsed_time:
-            buf.append("%.3f " % diff_time)
-        if self.show_level:
-            buf.append("{level}:  ".format(level=level.upper()))
-        if self.current_component:
-            buf.append("[{component}]  ".format(component=str(self.current_component)))
+        buf.append(self._output_prefix(level))
         buf.append(str(message))
 
         # support python3 chained exceptions
