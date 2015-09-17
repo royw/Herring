f041cd0945ca9ff287af6d1974d4abecedf30af4
royw <roy@wright.org>
2015-09-17 16:36:49 -0500

    switch to ReadTheDoc theme

--- herring/__init__.py
+++ herring/__init__.py
@@ -2,8 +2,8 @@
 
 # pylint: disable=C0301,W1401
 """
-Herring
-=======
+What is Herring?
+================
 
 Herring is a simple python make utility.  You write tasks in python, and
 optionally assign dependent tasks.  The command line interface lets you easily
@@ -15,8 +15,11 @@ slightly higher so you get a two layer effect with a little path running down
 the middle. ("A path! A path!") Then, you must cut down the mightiest tree in
 the forrest... with... a herring!"
 
-Usage
------
+Tasks
+=====
+
+Task Definition
+---------------
 
 Tasks are defined by using a @task decorator on a function definition in the
 project's herringfile::
@@ -301,61 +304,8 @@ To see a list of public tasks:
 
     ➤ herring -T
 
-
-Command line help is available
-==============================
-
-To display the help message::
-
-    ➤ herring/herring_main.py --help
-    usage: Herring [-h] [-c FILE] [-f FILESPEC] [--herringlib [DIRECTORY [DIRECTORY ...]]] [-T] [-U] [-D] [-a] [-q] [-d]
-                   [--herring_debug] [--leave_union_dir] [-j] [-v] [-l]
-                   [tasks [tasks ...]]
-
-    "Then, you must cut down the mightiest tree in the forrest... with... a herring!" Herring is a simple python make
-    utility. You write tasks in python, and optionally assign dependent tasks. The command line interface lets you
-    easily list the tasks and run them. See --longhelp for details.
-
-    optional arguments:
-      -h, --help                  show this help message and exit
-      -c FILE, --conf_file FILE   Configuration file in INI format (default: ['.herringrc',
-                                  '/home/wrighroy/.herring/herring.conf', '/home/wrighroy/.herringrc'])
-
-    Config Group:
-
-      -f FILESPEC, --herringfile FILESPEC
-                                  The herringfile name to use, by default uses "herringfile".
-      --herringlib [DIRECTORY [DIRECTORY ...]]
-                                  The location of the herringlib directory to use (default: ['herringlib',
-                                  '~/.herring/herringlib']).
-
-    Task Commands:
-
-      -T, --tasks                 Lists the public tasks (with docstrings).
-      -U, --usage                 Shows the full docstring for the tasks (with docstrings).
-      -D, --depends               Lists the tasks (with docstrings) with their dependencies.
-      tasks                       The tasks to run. If none specified, tries to run the 'default' task.
-
-    Task Options:
-
-      -a, --all                   Lists all tasks, even those without docstrings.
-
-    Output Options:
-
-      -q, --quiet                 Suppress herring output.
-      -d, --debug                 Display task debug messages.
-      --herring_debug             Display herring debug messages.
-      --leave_union_dir           Leave the union herringlib directory on disk (do not automatically erase). Useful for
-                                  debugging.
-      -j, --json                  Output list tasks (--tasks, --usage, --depends, --all) in JSON format.
-
-    Informational Commands:
-
-      -v, --version               Show herring's version.
-      -l, --longhelp              Long help about Herring.
-
 """
 
 __docformat__ = 'restructuredtext en'
 
-__version__ = '0.1.35'
+__version__ = '0.1.36'
