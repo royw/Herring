d4a06d49aef522752219f9682d4bd0987159ba37
royw <roy@wright.org>
2015-09-17 17:50:47 -0500

    specify bugzilla using example.com URL

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ project_author = u'Roy Wright'
 project_author_email = u'roy@wright.org'
 project_description = u'Just a python make utility.'
 project_package = u'herring'
-project_bugzilla_url = u'https://bugzilla.austin.hp.com'
+project_bugzilla_url = u'https://bugzilla.example.com'
 
 # -- General configuration -----------------------------------------------------
 
@@ -435,7 +435,7 @@ html_theme_path = ['_themes']
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 # html_logo = None
-html_logo = "Herring.gif"
+html_logo = "_static/Project_animated.gif"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
