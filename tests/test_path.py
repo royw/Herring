93ae5243aa037534a73141b97af407be1943e115
royw <roy_wright@trendmicro.com>
2016-03-16 10:28:21 -0500

    normalize path

--- tests/test_path.py
+++ tests/test_path.py
@@ -11,3 +11,16 @@ def test_comparisons():
     set_b = set([Path('foo', 'f1'), Path('foo', 'f2'), Path('foo', 'f3', 'f4')])
 
     assert sorted(set_a) == sorted(set_b)
+
+
+def test_slashes():
+    a_path = Path('/smsclient/jre', './foo/bar')
+    assert str(a_path) == '/smsclient/jre/foo/bar'
+    assert '/smsclient/jre/foo/bar' == str(a_path)
+    assert a_path.is_absolute()
+
+
+def test_slashes2():
+    a_path = Path('/smsclient/jre', 'foo/bar')
+    assert str(a_path) == '/smsclient/jre/foo/bar'
+    assert a_path.is_absolute()
